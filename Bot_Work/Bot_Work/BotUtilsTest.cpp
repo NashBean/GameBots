071283a1273237341d92ae66bd68944870726b81
NashBean <nashbean@gmail.com>
2015-11-30 13:11:11 -0500

    handy bot functions exaple

--- Bot_Work/Bot_Work/BotUtilsTest.cpp
+++ Bot_Work/Bot_Work/BotUtilsTest.cpp
@@ -1,5 +1,5 @@
 //
-//  BotIOFunctions.cpp
+//  BotUtilsTest.cpp
 //
 //
 //  Created by nash on 11/24/15.
@@ -7,4 +7,21 @@
 //  All rights reserved.
 //
 
-#include "BotIOFunctions.h"
+#include "BotUtls.h"
+
+struct Student
+{
+    int age;
+    int standard;
+    std::string first_name, last_name;
+    Student():age(-1), first_name (""), last_name (""), standard(-1){};
+    ~Student(){};
+    void in()
+    {
+        std::cin >> age >> first_name >> last_name >> standard;
+    };
+    void out()
+    {
+        std::cout << age << " " << first_name << " " << last_name << " " << standard;
+    };
+};
